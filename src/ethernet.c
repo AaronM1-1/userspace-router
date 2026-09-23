81d9ca1fdb8d7f7ce06595b57781ed8a1d893985
Aaron Matas <matasaaron80@gmail.com>
2026-09-22 19:27:10 -0700

    Plan to output packet in hex to view ethernet frame format so I can parse the packet correctly

--- src/ethernet.c
+++ src/ethernet.c
@@ -1,14 +1,14 @@
 #include <sys/socket.h>
 #include <linux/if_packet.h>
 #include <netinet/in.h>
-#include <../include/ethernet.h>
+#include "../include/ethernet.h"
 #include <errno.h>
 #include <stdio.h>
 #include <linux/if_ether.h>
 #include <stdlib.h>
 #include <string.h>
 #include <net/if.h>
-
+#include <unistd.h>
 
 int main() {
     
@@ -29,7 +29,19 @@ int main() {
     }
 
     
+    unsigned char buff[2048];
+    socklen_t addLen = sizeof(addr);
+    int dataRecv = 0;
+    if((dataRecv = recvfrom(socket_packet, buff, sizeof(buff), 0, (struct sockaddr *)&addr, &addLen)) == -1) {
+        fprintf(stderr, "Recv Error: %s\n", strerror(errno));
+        exit(1);
+    }
 
-
-
+    
+    for(int i = 0; i < dataRecv; i++) {
+        printf("%02x ", buff[i]);
+    }
+    printf("\n");
+   
+    close(socket_packet);
 }
