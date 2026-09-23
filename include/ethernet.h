2387364f4b2a4d757f46ca9d5d83baf34dfda3bb
Aaron Matas <matasaaron80@gmail.com>
2026-09-22 18:01:24 -0700

    create socket and bind to receive ethernet frame

--- include/ethernet.h
+++ include/ethernet.h
@@ -1,2 +1,14 @@
-//Ethernet Header
+#ifndef ETHERNET_H
+#define ETHERNET_H
+
+#include <stdint.h>
+
+struct eth_header {
+    char destAddress[6];
+    char sourceAddress[6];
+    uint16_t ethType;
+};
+
+
+#endif
 
--- src/ethernet.c
+++ src/ethernet.c
@@ -1,1 +1,35 @@
-//ethernet source file
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <netinet/in.h>
+#include <../include/ethernet.h>
+#include <errno.h>
+#include <stdio.h>
+#include <linux/if_ether.h>
+#include <stdlib.h>
+#include <string.h>
+#include <net/if.h>
+
+
+int main() {
+    
+    struct sockaddr_ll addr = {0};
+    addr.sll_family = AF_PACKET;
+    addr.sll_ifindex = if_nametoindex("enp4s0");
+    addr.sll_protocol = htons(ETH_P_ALL);
+
+    int socket_packet;
+    if((socket_packet = socket(AF_PACKET, SOCK_RAW, addr.sll_protocol)) == -1) {
+        fprintf(stderr, "Socket Error: %s\n", strerror(errno));
+        exit(1);
+    }
+   
+    if(bind(socket_packet, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        fprintf(stderr, "Bind Error: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    
+
+
+
+}
